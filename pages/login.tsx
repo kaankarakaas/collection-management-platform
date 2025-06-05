5ea832e59f3f8a6bafd8fd9b73aa1180e8bea7cc
Kaan Karakaş <80331183+kaankarakaas@users.noreply.github.com>
2025-06-06 00:04:21 +0300

    fix login flow

--- pages/login.tsx
+++ pages/login.tsx
@@ -36,8 +36,6 @@ export default function Login() {
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault()
 
-        return router.push("/collections");
-
         setError("")
         setDetailedErrors([])
 
